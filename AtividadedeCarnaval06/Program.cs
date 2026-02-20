948fc599c7abab7e2b1eebedda2dde781f4b46ed
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-02-20 19:57:34 -0300

    commit

---
+++ AtividadedeCarnaval06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+static class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a venda média mensal: ");
+        double vendaMedia = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o preço atual: R$ ");
+        double precoAtual = double.Parse(Console.ReadLine());
+
+        double novoPreco = precoAtual;
+
+        if (vendaMedia < 500 && precoAtual < 30.00)
+        {
+            novoPreco = precoAtual * 1.10;
+        }
+
+        else if (vendaMedia >= 500 && precoAtual >= 30.00)
+        {
+            novoPreco = precoAtual * 0.80;
+        }
+
+        Console.WriteLine("O novo preço do produto é: " + novoPreco + "R$");
+    }
+}
