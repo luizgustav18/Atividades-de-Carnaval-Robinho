9bfb074e92b0abb1cd7b77b32ae9e7a3ffb3f3af
Luiz Gustavo <luizgustavoms18@gmail.com>
2026-02-20 19:56:06 -0300

    commit

---
+++ Atividadedecarnaval04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+static class Program
+{
+    static void Main()
+    {
+        double s = 0;
+        double numerador = 1;
+
+        for (int denominador = 1; denominador <= 50; denominador++)
+        {
+            s += numerador / denominador;
+            numerador += 2;
+        }
+
+        Console.WriteLine("O valor final de S é: "+s);
+    }
+}
